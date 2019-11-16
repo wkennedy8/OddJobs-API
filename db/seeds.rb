19fe88e5d9af89f6e17ab938881e2b891dbed998
Don Morehouse <dm.wyncode@gmail.com>
2019-11-16 01:17:14 +0000

    modified:   db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(email: 'example@mail.com' , password: '123123123' , password_confirmation: '123123123')
+p user
+profile = Profile.create!(first_name: 'foo', last_name: 'bar', ex_email: user.email)
+cc = CreditCard.create!(card_number: '120983452309482309',
+    card_exp_date: "2023-10-01",
+    card_cvc_number: "483",
+    profile: profile)
+p cc 
